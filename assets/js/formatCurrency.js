8571872acda9ac101afa323325f2b3c3a7a587c4
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2018-08-19 17:25:12 +0100

    Add support for rounding to negative decimal places

--- assets/js/formatCurrency.js
+++ assets/js/formatCurrency.js
@@ -29,10 +29,23 @@ function formatCurrency(amount,
         if (typeof(number) != 'number') {
             number = parseFloat(number);
         }
-        // Increase the size of the number by (decimals) orders of magntude,
-        // use the built-in round function to round the result, and then reduce
-        // by (decimals) OOM again.
-        return Number(Math.round(number + 'e' + decimals) + 'e-' + decimals)
+
+        if (typeof(decimals) != 'number') {
+            decimals = parseInt(decimals);
+        }
+
+        // Allow positive and negative values for "decimals"
+        if (decimals < 0) {
+            decimals = Math.abs(decimals);
+            // Decrease the size of the number by (decimals) orders of magnitude,
+            // use built-in round function, then increase by (decimals) OOM.
+            return Number(Math.round(number + 'e-' + decimals) + 'e' + decimals);
+        } else {
+            // Increase the size of the number by (decimals) orders of magntude,
+            // use the built-in round function to round the result, and then reduce
+            // by (decimals) OOM again.
+            return Number(Math.round(number + 'e' + decimals) + 'e-' + decimals);
+        }
     }
 
     if (truncateMinorUnits) {

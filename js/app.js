e82e370205dc5d8ac96d3da051f7bd4aa0436c2a
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2018-08-19 11:11:14 +0100

    Refactor formatCurrency into its own module

--- js/app.js
+++ js/app.js
@@ -10,57 +10,8 @@ window.onload = function() {
         console.log(`Net: ${net}`);
         console.log(`Tip rate: ${tipRate * 100}%`);
         console.log(`Total: ${total}`);
-        document.getElementById("total-amount").innerHTML = `Total to pay: ${formatCurrency(total)}`;
+        document.getElementById("total-amount").innerHTML = `Net: ${formatCurrency(net)}<br>
+            Tip: ${formatCurrency(tip)}<br>
+            Total to pay: ${formatCurrency(total)}`;
     })
-}
-
-
-function formatCurrency(amount,
-                        prefix = '£',
-                        displayMinorUnitsLimit = 10000,
-                        majorSep = ',',
-                        decimalSep = '.')
-{
-
-    let major, minor;
-    let truncateMinorUnits = parseFloat(amount) >= displayMinorUnitsLimit;
-
-    // Define a rounding function to handle input like 123.4567.  Note that this
-    // will not insert decimals - 10 is not converted to 10.00
-    function round(number, decimals) {
-
-        if (typeof(number) != 'number') {
-            number = parseFloat(number);
-        }
-        return Number(Math.round(number + 'e' + decimals) + 'e-' + decimals)
-    }
-
-    amount = round(amount, 2);
-
-    // We need a string to split out major and minor units
-    if (typeof(amount) == 'number') {
-        amount = amount.toString();
-    }
-
-    // Split on decimal point if there is one
-    if (amount.includes('.')) {
-        amount = amount.split('.');
-        major = amount[0];           // Pounds, dollars
-        minor = amount[1];           // Pence, cents
-    } else {
-        major = amount;
-        minor = "00";
-    }
-
-    // Do we need to add separator formatting to major units? This regex asserts
-    // that a point is followed by three digits AND that it is followed by a
-    // number of digits that is divisible by three. If both conditions are met
-    // the major separator is inserted at that position.
-    major = major.replace(/\B(?=(\d{3})+(?!\d))/g, majorSep)
-
-    if (truncateMinorUnits) {
-        return `${prefix}${major}`
-    } else {
-        return `${prefix}${major}${decimalSep}${minor}`
-    }
 }
